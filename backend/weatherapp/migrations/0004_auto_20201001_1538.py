3b9eb9f08764638d658ea3ac7708c103efff40a4
Crowdbotics <team@crowdbotics.com>
2021-02-20 16:59:03 +0000

    Update models

--- backend/weatherapp/migrations/0004_auto_20201001_1538.py
+++ backend/weatherapp/migrations/0004_auto_20201001_1538.py
@@ -7,21 +7,35 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('weatherapp', '0003_locations_cloudiness'),
+        ("weatherapp", "0003_locations_cloudiness"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Cloudiness',
+            name="Cloudiness",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('minimum', models.CharField(max_length=256)),
-                ('maximum', models.CharField(max_length=256)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("minimum", models.CharField(max_length=256)),
+                ("maximum", models.CharField(max_length=256)),
             ],
         ),
         migrations.AlterField(
-            model_name='locations',
-            name='cloudiness',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='locations_cloudiness', to='weatherapp.Cloudiness'),
+            model_name="locations",
+            name="cloudiness",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="locations_cloudiness",
+                to="weatherapp.Cloudiness",
+            ),
         ),
     ]
